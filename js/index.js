6d29419ae5ae41fca1f314332e9ba0c8bf4da0b4
Burbuha <burbovskaja@gmail.com>
2020-11-26 06:52:45 +0300

    slider, link

--- js/index.js
+++ js/index.js
@@ -27,3 +27,47 @@ toTop.addEventListener("click", (e) => {
   e.preventDefault();
   home.scrollIntoView({ behavior: "smooth" });
 });
+
+//slider
+$(".carousel").carousel({
+  interval: 5000,
+});
+
+//advantages
+const advBtn = document.querySelectorAll(".card-adv_img");
+
+advBtn.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    let advInfo = item.previousElementSibling.lastElementChild;
+    advInfo.classList.toggle("adv-info__active");
+    item.classList.toggle("adv-item__active");
+  });
+});
+
+//map
+ymaps.ready(function () {
+  var myMap = new ymaps.Map(
+      "map",
+      {
+        center: [53.889865291208594, 27.575227855944892],
+        zoom: 18,
+      },
+      {
+        searchControlProvider: "yandex#search",
+      }
+    ),
+    myPlacemark = new ymaps.Placemark(
+      myMap.getCenter(),
+      {
+        hintContent: 'ООО "Сервис"',
+        balloonContent: 'ООО "Сервис"',
+      },
+      {
+        iconLayout: "default#image",
+        iconImageHref: "../image/marker.png",
+        iconImageSize: [75, 102],
+      }
+    );
+  myMap.geoObjects.add(myPlacemark);
+});
